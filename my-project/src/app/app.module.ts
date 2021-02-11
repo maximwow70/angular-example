9f54b069c7e80c9d52ed2b6e08095d0e61bd1686
Maksim Samuilionak <maximwow70@gmail.com>
2021-02-11 16:50:13 +0300

    prepare to lesson 4

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { HomeComponent } from './components/home/home.component';
 import { UserListModule } from './components/user-list/user-list.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactsComponent,
+    HomeComponent,
+    AboutMeComponent,
+    AboutUsComponent,
   ],
   imports: [
     BrowserModule,
